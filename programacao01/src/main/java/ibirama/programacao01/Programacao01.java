05b955124967ca00e70b631b4ca71f477c1c596e
aluno <aluno@lab01-30>
2026-03-02 09:03:33 -0300

    Teste

---
+++ programacao01/src/main/java/ibirama/programacao01/Programacao01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package ibirama.programacao01;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aluno
+ */
+public class Programacao01 {
+
+    private String nome;
+    private Date dataNascimento;
+    private String cpf;
+    private char genero;
+    private String matricula;
+    private String email;
+}
+
