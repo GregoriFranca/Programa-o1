b0b6474eed79bab0233f6de665aea796631758d8
aluno <aluno@lab01-30>
2026-03-23 08:57:37 -0300

    borameupovo

---
+++ Gregori_Franca/src/main/java/com/mycompany/gregori_franca/Disciplina.java
@@ -0,0 +1,88 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gregori_franca;
+
+/**
+ *
+ * @author aluno
+ */
+public class Disciplina {
+    private String codigo;
+    private String nome;
+    private String cargaHoraria;
+    private String ementa;
+    private String objetivo;
+   
+    //extras
+    private String objetivoApredizagem;
+    private String conteudoProgramatico;
+    private String referenciasBibliograficas;
+   
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String _nome) {
+        nome = _nome;
+    }
+   
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String _codigo) {
+        codigo = _codigo;
+    }
+   
+    public String getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(String _cargaHoraria) {
+        cargaHoraria = _cargaHoraria;
+    }
+   
+    public String getEmenta() {
+        return ementa;
+    }
+
+    public void setEmenta(String _ementa) {
+        ementa = _ementa;
+    }
+   
+    public String getObjetivo() {
+        return objetivo;
+    }
+
+    public void setOjetivo(String _ojetivo) {
+        objetivo = _ojetivo;
+    }
+   
+    public String getObjetivoApredizagem() {
+        return objetivoApredizagem;
+    }
+
+    public void setObjetivoApredizagem(String _objetivoApredizagem) {
+        objetivoApredizagem = _objetivoApredizagem;
+    }
+   
+    public String getConteudoProgramatico() {
+        return conteudoProgramatico;
+    }
+
+    public void setConteudoProgramatico(String _conteudoProgramatico) {
+        conteudoProgramatico = _conteudoProgramatico;
+    }
+   
+    public String getReferenciasBibliograficas() {
+        return referenciasBibliograficas;
+    }
+
+    public void setReferenciasBibliograficas(String _referenciasBibliograficas) {
+        referenciasBibliograficas = _referenciasBibliograficas;
+    }
+   
+}
--- Gregori_Franca/src/main/java/com/mycompany/gregori_franca/Professor.java
+++ Gregori_Franca/src/main/java/com/mycompany/gregori_franca/Professor.java
@@ -1,25 +1,71 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.gregori_franca;
 
-/**
- *
- * @author aluno
- */
+import java.util.Date;
+
 public class Professor {
-    //Professor
     private String nome;
-    private int numeroIndentificacao;
-    private String enderecoEletronico;
-    private String telefone;
-    private String titulacaoAcademica;
+    private String email;
     private String areaAtuacao;
     private String regimeTrabalho;
-    private String dataIngresso;
-    private String formacao;
+    private String tituloAcademico;
+    private String formacao;  
+    private String telefone;
+    private String siape;
+    private Date dataIgresso;
     
-    
-    
+    public  String getNome(){
+        return nome;
+    }
+    public void setNome(String _nome){
+        nome = _nome;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String _Email){
+        email = _Email;
+    }
+    public String getAreaAtucao(){
+        return areaAtuacao;
+    }
+    public void setAreaAtuacao(String _areaAtuacao){
+        areaAtuacao = _areaAtuacao;
+    }
+    public  String getregimeTra(){
+        return regimeTrabalho;
+    }
+    public void setregimeTra(String _regimeTrabalho){
+        regimeTrabalho = _regimeTrabalho;
+    }
+    public  String getTituloAcademico(){
+        return tituloAcademico;
+    }
+    public void setTituloAcademico(String _tituloAcademico){
+        regimeTrabalho = _tituloAcademico;
+    }
+    public  String getformacao(){
+        return formacao;
+    }
+    public void setformacao(String _formacao){
+        formacao = _formacao;
+    }
+    public  String getTelefone(){
+        return telefone;
+    }
+    public void setTelefone(String _telefone){
+        telefone = _telefone;
+    }
+    public void setSiape(String _siape) {
+        siape = _siape;
+    }
+    public String getSiape() {
+        return siape;
+    }
+   
+    public Date getDataIgresso() {
+        return dataIgresso;
+    }
+    public void setDataIngresso(Date _dataIngresso) {
+        dataIgresso = _dataIngresso;
+    }
 }
